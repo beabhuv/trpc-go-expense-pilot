bfb18390ae54732dac5d2cc0250c7abe75c035ca
bvenkatr10 <bv10@illinois.edu>
2025-06-04 23:09:29 -0700

    Backup before destroy

--- expense-tool/infra/main.tf
+++ expense-tool/infra/main.tf
@@ -15,15 +15,33 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
 
-  name               = "expense-tool-vpc"
-  cidr               = "10.0.0.0/16"
-  azs                = ["us-east-1a", "us-east-1b"]
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
+  name = "expense-tool-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
   enable_nat_gateway = true
   single_nat_gateway = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = module.vpc.vpc_id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                        = "public-${var.azs[count.index]}"
+    "kubernetes.io/role/elb"    = "1"
+    "kubernetes.io/cluster/expense-tool-eks" = "shared"
+  }
 }
 
 module "eks" {
