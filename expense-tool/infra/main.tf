ac2eaca154133d0e173247c1094c3ab6b9089075
bvenkatr10 <bv10@illinois.edu>
2025-06-04 18:43:57 -0700

    added CI

--- expense-tool/infra/main.tf
+++ expense-tool/infra/main.tf
@@ -1,18 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
+  version = "~> 5.0"
 
-  name = "expense-tool-vpc"
-  cidr = "10.0.0.0/16"
-  azs  = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  name               = "expense-tool-vpc"
+  cidr               = "10.0.0.0/16"
+  azs                = ["us-east-1a", "us-east-1b"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
   enable_nat_gateway = true
   single_nat_gateway = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 module "eks" {
@@ -20,16 +31,17 @@ module "eks" {
   version = "~> 18.0"
 
   cluster_name    = "expense-tool-eks"
-  cluster_version = "1.27"
+  cluster_version = "1.28"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     default = {
-      min_size     = 2
-      max_size     = 3
-      desired_size = 2
-      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+      instance_types = ["t3.medium", "t3a.medium"]
+      disk_size = 20
     }
   }
 }
